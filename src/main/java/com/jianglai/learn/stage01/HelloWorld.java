2058511b6fc973ec164a977164370d8c1b1271c7
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-20 21:05:21 +0800

    fix: update Hello java!!!

--- src/main/java/com/jianglai/learn/stage01/HelloWorld.java
+++ src/main/java/com/jianglai/learn/stage01/HelloWorld.java
@@ -2,7 +2,8 @@ package com.jianglai.learn.stage01;  // 包声明
 
 public class HelloWorld {    // 类声明，Java所有代码必须写在类里面。类是Java的最小组织单位
     public static void main(String[] args) {   // 主方法（整个程序的入口）
-        System.out.println("Hello, Java!");  // 打印语句
+        System.out.println("Hello, Java!");  // 打印语句（输出并换行）
+        System.out.print("Hello, Java!");  // 打印语句（输出但不换行）
     }
 }
 
